234fdb328f871e1266f373301665f9db171f9a44
Luciano <pablo26alves@gmail.com>
2024-08-28 11:40:59 -0300

    Adicionado a função Honeypot. Sistema funcionando normalmente.

--- app/Config/Honeypot.php
+++ app/Config/Honeypot.php
@@ -14,12 +14,12 @@ class Honeypot extends BaseConfig
     /**
      * Honeypot Label Content
      */
-    public string $label = 'Fill This Field';
+    public string $label = 'Adicione um texto';
 
     /**
      * Honeypot Field Name
      */
-    public string $name = 'honeypot';
+    public string $name = 'Texto1';
 
     /**
      * Honeypot HTML Template
--- app/Views/pages/sobre.php
+++ app/Views/pages/sobre.php
@@ -1,1 +1,12 @@
-<p>Conteúdo da Página Sobre</p>
+<div class="container">
+    <p>Conteúdo da Página Sobre</p>
+
+    <?php
+        $texto = "Mem Vindo ao Codeigniter 4!";
+        $textoCripto = $cripto->encrypt($texto);
+        echo "Mensagem Criptografada:<br>";
+        echo $textoCripto.'<br><br>';
+        echo "Mensagem Descriptografada:<br>";
+        echo $cripto->decrypt($textoCripto);
+    ?>
+</div>
